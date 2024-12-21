0f52bb98f06f4bb8a41941878b7686804931cb6c
Yamini1107 <138500013+Yamini1107@users.noreply.github.com>
2024-12-21 20:41:15 +0530

    Add files via upload

---
+++ Library Management  System/connection.php
@@ -0,0 +1,5 @@
+<?php
+
+     $db=mysqli_connect("localhost","root","","library");   /*  server name,username,password,database name */
+     mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
+?>
